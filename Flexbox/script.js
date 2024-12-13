5e56ee1ce698c2b6b952c3ba992a3df7552b7638
Joryh-IO <sujeethvizag@gmail.com>
2024-12-13 20:44:56 +0530

    made it work somehow

--- Flexbox/script.js
+++ Flexbox/script.js
@@ -1,6 +1,33 @@
 //window.alert("hello mf");
-function f(){
-    var a = document.getElementsByClassName("items")[0];
-    a.getElementsByClassName("items")[0].sty
+var a = document.getElementsByClassName("items")[0]
+var b = document.querySelectorAll(".toggle button");
+
+function tog(event){
+    console.log(event);
+    if(b[0].style.cursor =="pointer"){
+        for(i=0;i<b.length;i++){
+            b[i].style.cursor = "not-allowed";
+            b[i].style.opacity = 0.5;
+        }
+    }else{
+        for(i=0;i<b.length;i++){
+            b[i].style.cursor = "pointer";
+            b[i].style.opacity = 1;
+        }
+    }
 }
-console.log(a);
+
+function f(ele){
+    a.style.justifyContent=ele;
+    console.log(a);
+}
+
+function f2(ele){
+    a.style.flexDirection = ele;
+    console.log(a);
+}
+
+function f3(ele){
+    a.style.alignItems = ele;
+    console.log(a);
+}
