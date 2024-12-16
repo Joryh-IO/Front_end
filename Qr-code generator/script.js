5919abbdae0334f8014279e423afc12dfa8077cd
Joryh-IO <sujeethvizag@gmail.com>
2024-12-16 19:15:02 +0530

    made a draft but has a reference error

---
+++ Qr-code generator/script.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const qr = require("qr-image");
+var t = "https://www.youtube.com/watch?v=2X_2IdybTV0";
+
+function gen(){
+    fs.unlink("generated.svg",(err)=>{
+        if(err)console.log("error deleting file");
+        else console.log("done deletion");
+    });
+    var t = document.getElementById("textbox").value;
+    var qr_svg = qr.image(t, { type: 'svg' });
+    qr_svg.pipe(require('fs').createWriteStream('generated.svg'));
+    var svg_string = qr.imageSync(t, { type: 'svg' });
+}
